536544b30aced33e6e89ef9805bda02f29c4f02b
Bullets-jyr <jyr@bullets.co.kr>
2023-11-23 23:23:58 +0900

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by xinapse on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
